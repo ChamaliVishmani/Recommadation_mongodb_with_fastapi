481f9e2b6329c5952f1043f2d0b4b2f27f83d2de
Thimira Nirmal <timnirmal@gmail.com>
2023-04-23 20:00:06 +0530

    Before remove model

--- sentiment.py
+++ sentiment.py
@@ -1,12 +1,9 @@
 import warnings
-
-warnings.filterwarnings('ignore')
 from keras.models import Sequential
 from keras import layers
 from keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from keras.callbacks import ModelCheckpoint
-
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 import pickle
 import re
 import gensim
@@ -16,18 +13,18 @@ import tensorflow as tf
 import keras
 import numpy as np
 import pandas as pd
-
 import nltk
-
-nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
-
-nltk.download('stopwords')
 from nltk.corpus import stopwords
-
-nltk.download('wordnet')
 from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+
+warnings.filterwarnings('ignore')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+wordnet_lemmatizer = WordNetLemmatizer()
 
 # POS tagger dictionary
 pos_dict = {'J': wordnet.ADJ, 'V': wordnet.VERB, 'N': wordnet.NOUN, 'R': wordnet.ADV}
@@ -35,18 +32,10 @@ pos_dict = {'J': wordnet.ADJ, 'V': wordnet.VERB, 'N': wordnet.NOUN, 'R': wordnet
 # show all columns
 pd.set_option('display.max_columns', None)
 
-from nltk.stem import WordNetLemmatizer
-
-wordnet_lemmatizer = WordNetLemmatizer()
-
 
 def clean_text(text):
     text = re.sub('[^A-Za-z]+', ' ', text)
-    text = text.strip()
-    text = text.split()
-    text = [word for word in text if word not in stopwords.words('english')]
-    text = ' '.join(text)
-
+    text = ' '.join([word for word in text.strip().split() if word not in stopwords.words('english')])
     return text
 
 
@@ -88,6 +77,19 @@ def process_data():
     df.to_csv("data/sentiment/lemma.csv", index=False)
 
 
+def clean_chars(data):
+    # Remove new line characters
+    data = re.sub('\s+', ' ', data)
+    # Remove distracting single quotes
+    data = re.sub("\'", "", data)
+    return data
+
+
+def sentence_to_words(sentences):
+    for sentence in sentences:
+        yield gensim.utils.simple_preprocess(str(sentence), deacc=True)
+
+
 def train_sentiment():
     # load csv
     df = pd.read_csv("data/sentiment/lemma.csv")
@@ -108,49 +110,24 @@ def train_sentiment():
     df["selected_text"].isnull().sum()
     df["selected_text"].fillna("No content", inplace=True)
 
-    def depure_data(data):
-        # Removing URLs with a regular expression
-        url_pattern = re.compile(r'https?://\S+|www\.\S+')
-        data = url_pattern.sub(r'', data)
-
-        # Remove Emails
-        data = re.sub('\S*@\S*\s?', '', data)
-
-        # Remove new line characters
-        data = re.sub('\s+', ' ', data)
-
-        # Remove distracting single quotes
-        data = re.sub("\'", "", data)
-
-        return data
-
     temp = []
     # Splitting pd.Series to list
     data_to_list = df['selected_text'].values.tolist()
     for i in range(len(data_to_list)):
-        temp.append(depure_data(data_to_list[i]))
+        temp.append(clean_chars(data_to_list[i]))
     list(temp[:5])
 
-    def sent_to_words(sentences):
-        for sentence in sentences:
-            yield (gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
-
-    data_words = list(sent_to_words(temp))
-
-    print(data_words[:10])
-
-    def detokenize(text):
-        return TreebankWordDetokenizer().detokenize(text)
+    data_words = list(sentence_to_words(temp))
 
     data = []
     for i in range(len(data_words)):
-        data.append(detokenize(data_words[i]))
+        data.append(TreebankWordDetokenizer().detokenize(data_words[i]))
     print(data[:5])
 
     data = np.array(data)
     print(data[:5])
 
-    ##### Label encoding
+    # Label encoding
     labels = np.array(df['sentiment'])
     y = []
     for i in range(len(labels)):
@@ -164,20 +141,17 @@ def train_sentiment():
     labels = tf.keras.utils.to_categorical(y, 3, dtype="float32")
     del y
 
-    #### Data sequencing and splitting
+    # Data sequencing and splitting
     max_words = 5000
     max_len = 200
 
     tokenizer = Tokenizer(num_words=max_words)
     tokenizer.fit_on_texts(data)
     sequences = tokenizer.texts_to_sequences(data)
-    tweets = pad_sequences(sequences, maxlen=max_len)
-    print(tweets)
-
-    print(labels)
+    features = pad_sequences(sequences, maxlen=max_len)
 
     # Splitting the data
-    X_train, X_test, y_train, y_test = train_test_split(tweets, labels, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, random_state=0)
     print(len(X_train), len(X_test), len(y_train), len(y_test))
 
     print(X_train.shape)
@@ -198,15 +172,16 @@ def train_sentiment():
     # model
     model1 = Sequential()
     model1.add(layers.Embedding(max_words, 20))
-    model1.add(layers.LSTM(15, dropout=0.5))
+    model1.add(layers.LSTM(20, dropout=0.5))
     model1.add(layers.Dense(3, activation='softmax'))
 
     model1.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-    # Implementing model checkpoins to save the best metric and do not lose it on training.
-    checkpoint1 = ModelCheckpoint("data/sentiment/model1_chk/best_model1.hdf5", monitor='val_accuracy', verbose=1,
-                                  save_best_only=True, mode='auto',
-                                  period=1, save_weights_only=False)
-    history = model1.fit(X_train, y_train, epochs=70, validation_data=(X_test, y_test), callbacks=[checkpoint1])
+
+    checkpoint1 = ModelCheckpoint("data/sentiment/model1_chk/best_model1.hdf5",
+                                  monitor='val_accuracy', verbose=1, save_best_only=True, mode='auto', period=1,
+                                  save_weights_only=False)
+    early = EarlyStopping(monitor="val_accuracy", mode="max", patience=20)
+    model1.fit(X_train, y_train, epochs=70, validation_data=(X_test, y_test), callbacks=[checkpoint1, early])
 
     # Bidirectional LTSM model
     model2 = Sequential()
@@ -214,12 +189,10 @@ def train_sentiment():
     model2.add(layers.Bidirectional(layers.LSTM(20, dropout=0.6)))
     model2.add(layers.Dense(3, activation='softmax'))
     model2.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-    # Implementing model checkpoins to save the best metric and do not lose it on training.
-    checkpoint2 = ModelCheckpoint("data/sentiment/model1_chk/best_model2.hdf5", monitor='val_accuracy', verbose=1, save_best_only=True,
-                                  mode='auto',
-                                  period=1, save_weights_only=False)
-    history = model2.fit(X_train, y_train, epochs=70, validation_data=(X_test, y_test), callbacks=[checkpoint2])
 
+    checkpoint2 = ModelCheckpoint("data/sentiment/model1_chk/best_model2.hdf5", monitor='val_accuracy', verbose=1,
+                                  save_best_only=True, mode='auto', period=1, save_weights_only=False)
+    model2.fit(X_train, y_train, epochs=70, validation_data=(X_test, y_test), callbacks=[checkpoint2])
 
     # save the model
     model1.save('data/sentiment/model/model1.h5')
@@ -228,18 +201,6 @@ def train_sentiment():
     # save model weights
     model1.save_weights('data/sentiment/model_weights/model1_weights.h5')
     model2.save_weights('data/sentiment/model_weights/model2_weights.h5')
-
-    print("-" * 50)
-    print("-" * 50)
-    print("-" * 50)
-    print("-" * 50)
-    print("-" * 50)
-
-    print("Done Training")
-
-    # Let's load the best model obtained during training
-    best_model = keras.models.load_model("data/sentiment/model_weights/best_model2.hdf5")
-    best_model.summary()
 
 
 def predict_sentiment(text):
@@ -264,17 +225,13 @@ def predict_sentiment(text):
 def pre_process(df):
     # drop null values
     df = df.dropna()
-
     # drop duplicates
     df = df.drop_duplicates()
 
     # keep only column_name, value
     df = df[['column_name', 'value']]
-
     # rename to selected_text	sentiment
     df = df.rename(columns={'column_name': 'sentiment', 'value': 'selected_text'})
-
-    print(df.tail())
 
     df["selected_text"].isnull().sum()
     df["selected_text"].fillna("No content", inplace=True)
@@ -282,33 +239,7 @@ def pre_process(df):
     return df
 
 
-def depure_data(data):
-    # Removing URLs with a regular expression
-    url_pattern = re.compile(r'https?://\S+|www\.\S+')
-    data = url_pattern.sub(r'', data)
-
-    # Remove Emails
-    data = re.sub('\S*@\S*\s?', '', data)
-
-    # Remove new line characters
-    data = re.sub('\s+', ' ', data)
-
-    # Remove distracting single quotes
-    data = re.sub("\'", "", data)
-
-    return data
-
-
-def sent_to_words(sentences):
-    for sentence in sentences:
-        yield (gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
-
-
-def detokenize(text):
-    return TreebankWordDetokenizer().detokenize(text)
-
-
 if __name__ == '__main__':
-    # process_data()
-    # train_sentiment()
+    process_data()
+    train_sentiment()
     predict_sentiment('The restaurant provided updates on the status of our order, which was very helpful.')
